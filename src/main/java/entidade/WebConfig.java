6d354d05fef8ac74b19244fb5add91fdc34dbdd6
Lucas Dröse Ribeiro <lucasdroseribeiro@gmail.com>
2019-07-05 00:22:41 -0300

    agora vai

--- src/main/java/entidade/WebConfig.java
+++ src/main/java/entidade/WebConfig.java
@@ -3,12 +3,12 @@ package entidade;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SuppressWarnings("deprecation")
 @Configuration
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
